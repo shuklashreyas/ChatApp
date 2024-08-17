cbacd72539dc06e4a5bb56dd52efa13660a61851
shreyman21 <shuklashreyas@icloud.com>
2024-08-17 23:03:11 +0800

    ui design cont

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Login from './pages/login/login'
 
 function App() {
 
   return <div className='p-4 h-screen flex items-center justify-center'>
 
     <Login/>
-
+  </div>;
   
 }
 
