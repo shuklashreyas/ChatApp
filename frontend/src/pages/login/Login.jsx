14d4e7a31f4a4754485e05e2f8bd9e668ff3811f
shreyman21 <shuklashreyas@icloud.com>
2024-08-17 23:37:09 +0800

    login button ui

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
-const login = () => {
+const Login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full-p-5 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>Login</h1>
+      </div>
+    </div>
+  );
+};
 
-export default login
+export default Login;
