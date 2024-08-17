495e78679b917ad68df0a20d6d023572370ae5b8
shreyman21 <shuklashreyas@icloud.com>
2024-08-17 18:24:18 +0800

    Sidebar Enpoint complete

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express(); 
 const PORT = process.env.PORT || 5001;
@@ -16,6 +20,7 @@ app.use(cookieParser()); // to parse cookies
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 // app.get("/", (req, res) => {
 //     root route http://localhost:5001/
