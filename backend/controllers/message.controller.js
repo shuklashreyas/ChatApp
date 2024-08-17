3fcda82bd11477ca1f178b8ef4855cec44c0013d
shreyman21 <shuklashreyas@icloud.com>
2024-08-17 17:21:35 +0800

    modified models folder

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,38 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+
 export const sendMessage = async (req, res) => {
     try{
         const{message} = req.body;
-        const {id} = req.params;
-        const senderId = req.userId;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+
+        })
+
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            });
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMesasge){
+            conversation.messages.push(newMessage._id);
+        }
+
+        res.status(201).json({message: "Message sent successfully"});
     }
+
+   
     catch(error){
         console.log("Error on sending message: ", error.message);
         res.status(500).json({error:"Internal Server Error"});
