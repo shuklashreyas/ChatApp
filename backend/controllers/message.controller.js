83039b8f8128cc8e6954c353d1dc0320a5e011c8
shreyman21 <shuklashreyas@icloud.com>
2024-08-17 17:46:22 +0800

    Get Messages Endpoint prog

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -11,7 +11,7 @@ export const sendMessage = async (req, res) => {
         let conversation = await Conversation.findOne({
             participants: { $all: [senderId, receiverId] },
 
-        })
+        });
 
         if(!conversation){
             conversation = await Conversation.create({
@@ -23,16 +23,39 @@ export const sendMessage = async (req, res) => {
             senderId,
             receiverId,
             message,
-        })
+        });
 
-        if(newMesasge){
+        if(newMessage){
             conversation.messages.push(newMessage._id);
         }
 
-        res.status(201).json({message: "Message sent successfully"});
+        //Socket.io functionality will go here
+
+        // await conversation.save();
+        // await newMessage.save();
+        await Promise.all([conversation.save(), newMessage.save()]); // run in parallel
+
+        res.status(201).json(newMessage);
     }
 
    
+    catch(error){
+        console.log("Error on sending message: ", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try{
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+    }
     catch(error){
         console.log("Error on sending message: ", error.message);
         res.status(500).json({error:"Internal Server Error"});
