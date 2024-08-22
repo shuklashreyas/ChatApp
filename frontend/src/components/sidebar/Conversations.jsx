dcb0cf1847b5f3826d1714aab29aaa50efed99b2
shreyman21 <shuklashreyas@icloud.com>
2024-08-23 01:34:45 +0800

    Get Convo Prog

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Conversation from './Conversation'
 
 const Conversations = () => {
+  useGetConversations();
     return (
         <div className = 'py-2 flex flex-col overflow-auto'>
         <Conversation />
