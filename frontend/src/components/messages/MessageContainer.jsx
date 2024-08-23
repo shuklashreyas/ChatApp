62f041fb84fc10f1602c77596945e83bb6c11624
shreyman21 <shuklashreyas@icloud.com>
2024-08-23 17:46:02 +0800

    send message functionality prog

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -12,13 +12,15 @@ const MessageContainer = () => {
     return () => {
       setSelectedConversation(null);
     }
-  },[setSelectedConversation])
+  },[setSelectedConversation]);
 
   return (
     <div className='md:min-w-[450px] flex flex-col'>
       {!selectedConversation ? (
-        console.log('No chat selected'),
-        <NoChatSelected />
+        <>
+          console.log('No chat selected');
+          <NoChatSelected />
+        </>
       ) : (
         <>
           <div className='bg-slate-500 px-4 py-2 mb-2'>
