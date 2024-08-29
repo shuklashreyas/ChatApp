7b4c25405ac01a03c4b2ff6a684641a83c57ce8a
shreyman21 <shuklashreyas@icloud.com>
2024-08-29 19:17:30 +0800

    fix login import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
-import Login from './pages/login/login'
+import Login from './pages/login/Login'
 import Signup from './pages/signup/SignUp'
 import { Toaster } from 'react-hot-toast'
 import { useAuthContext } from './context/AuthContext'
