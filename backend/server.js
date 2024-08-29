f258265a6dd8864604fa910ced12690ceabeaee1
shreyman21 <shuklashreyas@icloud.com>
2024-08-29 19:08:06 +0800

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,6 +11,7 @@ import { app, server } from "./socket/socket.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+const __dirname = path.resolve();
 
 
 const PORT = process.env.PORT || 5001; 
@@ -22,6 +24,12 @@ app.use(cookieParser()); // to parse cookies
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //     root route http://localhost:5001/
